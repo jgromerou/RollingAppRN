fde5d939b43948654f9eddf11851a267fbf61941
Cristian Quiroga <cristianq3@gmail.com>
2023-11-13 15:55:50 -0300

    Se acomodan cards en Categories con componente TouchableOpacity

--- src/screens/categoriesScreen.jsx
+++ src/screens/categoriesScreen.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { View, Text, FlatList, Image  } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 import { globalThemes } from '../themes/globalThemes';
 import { Card } from 'react-native-paper';
 
-const CategoriesScreen = ({navigation}) => {
-
-
+const CategoriesScreen = ({ navigation }) => {
   const categories = [
-    {  nombre: 'Zapatillas', imagen: 'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699545410/react-avanzado/uio0g9ppfjrdl9lbyk5c.jpg' },
-    {  nombre: 'Buzo', imagen: 'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699550793/react-avanzado/bgv3dadkc2zoma8mucgl.jpg' },
-    { nombre: 'Shorts', imagen: 'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699552814/react-avanzado/bi6li8dy1solckh2jxoz.jpg' },
-    { nombre: 'Remera', imagen: 'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699545979/react-avanzado/cfgwh5bwx5e11u9wfmni.jpg' },
+    {
+      nombre: 'Zapatillas',
+      imagen:
+        'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699545410/react-avanzado/uio0g9ppfjrdl9lbyk5c.jpg',
+    },
+    {
+      nombre: 'Buzo',
+      imagen:
+        'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699550793/react-avanzado/bgv3dadkc2zoma8mucgl.jpg',
+    },
+    {
+      nombre: 'Shorts',
+      imagen:
+        'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699552814/react-avanzado/bi6li8dy1solckh2jxoz.jpg',
+    },
+    {
+      nombre: 'Remera',
+      imagen:
+        'https://res.cloudinary.com/dvcq6vatc/image/upload/v1699545979/react-avanzado/cfgwh5bwx5e11u9wfmni.jpg',
+    },
     // ... más categorías
   ];
 
@@ -20,25 +34,43 @@ const CategoriesScreen = ({navigation}) => {
   };
 
   return (
-
     // Titulo del View
     <View>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginTop: 20 }}>Categorias</Text>
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginVertical: 20,
+        }}
+      >
+        Selecciona tu categoria favorita
+      </Text>
       <FlatList
         data={categories}
-       
         renderItem={({ item }) => (
-          <TouchableWithoutFeedback onPress={() => navigatetoProductos(item.nombre)}>
-          <Card elevation={4}>
-              <Image
-              source={{ uri: item.imagen }}
-              style={{ width: '100%', height: 200 }} // Ajusta el estilo según tus necesidades
-            />
-            <Text>{item.nombre}</Text>
+          // <TouchableWithoutFeedback onPress={() => navigatetoProductos(item.nombre)}>
 
+          <TouchableOpacity
+            style={{ ...globalThemes.menuButton}}
+            onPress={() => navigation.navigate('CategoriesScreen')}
+          >
+            <Card elevation={4} style={{position:"relative"}}>
+              <Image
+                source={{ uri: item.imagen }}
+                style={{ width: '100%', height: 500, objectFit:"cover" }} // Ajusta el estilo según tus necesidades
+              />
+              <Text  style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          position:"absolute",
+          left:"45%",
+          top:"40%"
           
-          </Card>
-          </TouchableWithoutFeedback>
+        }}>{item.nombre}</Text>
+            </Card>
+          </TouchableOpacity>
         )}
       />
     </View>
