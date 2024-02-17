3f59b4d88f9fbc9d13d489048eb4ba079eaf8cd8
Nicolas Herrera <herreranicoo17@gmail.com>
2024-02-17 13:32:19 -0300

    Se deshabilito el boton guardar en caso de que no se hayan completado todos los campos, y se agrego loading spinner al momento del submit

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import {
   Animated,
   Button,
@@ -8,16 +8,17 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
-import { globalThemes } from '../themes/globalThemes';
-import { ThemeContext } from '../contexts/ThemeContext';
-import { AuthContext } from '../contexts/AuthContext';
-import { ErrorMessage } from '../components/ErrorMessage';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+} from "react-native";
+import { globalThemes } from "../themes/globalThemes";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
+import { ErrorMessage } from "../components/ErrorMessage";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-import { AlertToast } from '../components/AlertToast';
-import { useToast } from '../hooks/useToast';
+import { AlertToast } from "../components/AlertToast";
+import { useToast } from "../hooks/useToast";
+import { CustomLoading } from "../components/CustomLoading";
 
 export const RegisterScreen = ({ navigation }) => {
   const { state } = useContext(ThemeContext);
@@ -25,28 +26,32 @@ export const RegisterScreen = ({ navigation }) => {
 
   const formik = useFormik({
     initialValues: {
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
     },
     validationSchema: Yup.object({
       firstname: Yup.string()
-        .required('Este campo es obligatorio')
-        .min(3, 'El nombre no debe tener menos de 3 caracteres')
-        .max(20, 'El nombre no debe tener más de 20 caracteres'),
+        .required("Este campo es obligatorio")
+        .min(3, "El nombre no debe tener menos de 3 caracteres")
+        .max(20, "El nombre no debe tener más de 20 caracteres"),
       lastname: Yup.string()
-        .required('Este campo es obligatorio')
-        .min(3, 'El apellido no debe tener menos de 3 caracteres')
-        .max(20, 'El apellido no debe tener más de 20 caracteres'),
+        .required("Este campo es obligatorio")
+        .min(3, "El apellido no debe tener menos de 3 caracteres")
+        .max(20, "El apellido no debe tener más de 20 caracteres"),
       email: Yup.string()
-        .email('Formato de email es incorrecto')
-        .required('Este campo es obligatorio'),
+        .email("Formato de email es incorrecto")
+        .matches(
+          /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+          "Ingresa una dirección de correo electrónico válida"
+        )
+        .required("Este campo es obligatorio"),
       password: Yup.string()
-        .required('Este campo es obligatorio')
+        .required("Este campo es obligatorio")
         .matches(
           /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,}$/,
-          'La contraseña debe tener mìnimo 8 caracteres , un n°, una minúscula, una mayúscula y no contener caracteres especiales.'
+          "La contraseña debe tener mìnimo 8 caracteres , un n°, una minúscula, una mayúscula y no contener caracteres especiales."
         ),
     }),
 
@@ -57,23 +62,21 @@ export const RegisterScreen = ({ navigation }) => {
     },
   });
 
-
   const [status, setStatus] = useState(null);
   const { popIn, popAnim } = useToast();
 
   useEffect(() => {
     if (stateAuth.successRegister) {
-      setStatus('success');
+      setStatus("success");
       popIn();
       setTimeout(() => {
-        navigation.replace('LoginScreen');
-      }, 1000);
+        navigation.replace("LoginScreen");
+      }, 1500);
     }
   }, [stateAuth.successRegister]);
 
   return (
     <>
-  
       {status && (
         <Animated.View
           style={[
@@ -83,117 +86,147 @@ export const RegisterScreen = ({ navigation }) => {
             },
           ]}
         >
-          <AlertToast status={status} titulo={'registró'} />
+          <AlertToast status={status} titulo={"registró"} />
         </Animated.View>
       )}
-
-      <View>
-        <Text style={[globalThemes.title, { color: state.colors.titleColor }]}>
-          Bienvenid@s
-        </Text>
-      </View>
-      <View>
-        <Text style={[globalThemes.text, { color: state.colors.text }]}>
-          Por favor registrese para continuar navegando en nuestra app.
-        </Text>
-      </View>
-      <View style={state.container}>
+      {status ? (
+        <CustomLoading />
+      ) : (
         <View>
-          <TextInput
-            style={[
-              globalThemes.defaultInputText,
-              { color: state.colors.text, borderColor: state.colors.border },
-            ]}
-            maxLength={20}
-            placeholder="Nombre"
-            placeholderTextColor={state.colors.notification}
-            name="firstname"
-            value={formik.values.firstname}
-            onChangeText={(value) => formik.setFieldValue('firstname', value)}
-          />
-          {formik.errors.firstname && (
-            <ErrorMessage message={formik.errors.firstname} />
-          )}
-          <TextInput
-            style={[
-              globalThemes.defaultInputText,
-              { color: state.colors.text, borderColor: state.colors.border },
-            ]}
-            maxLength={20}
-            placeholder="Apellido"
-            placeholderTextColor={state.colors.notification}
-            name="lastname"
-            value={formik.values.lastname}
-            onChangeText={(value) => formik.setFieldValue('lastname', value)}
-          />
-          {formik.errors.lastname && (
-            <ErrorMessage message={formik.errors.lastname} />
-          )}
-          <TextInput
-            style={[
-              globalThemes.defaultInputText,
-              { color: state.colors.text, borderColor: state.colors.border },
-            ]}
-            maxLength={40}
-            placeholder="Correo"
-            placeholderTextColor={state.colors.notification}
-            keyboardType="email-address"
-            name="email"
-            value={formik.values.email}
-            onChangeText={(value) => formik.setFieldValue('email', value)}
-          />
-          {formik.errors.email && (
-            <ErrorMessage message={formik.errors.email} />
-          )}
-
-          <TextInput
-            style={[
-              globalThemes.defaultInputText,
-              { color: state.colors.text, borderColor: state.colors.border },
-            ]}
-            maxLength={30}
-            placeholder="Contraseña"
-            placeholderTextColor={state.colors.notification}
-            secureTextEntry={true}
-            name="password"
-            value={formik.values.password}
-            onChangeText={(value) => formik.setFieldValue('password', value)}
-          />
-          {formik.errors.password && (
-            <ErrorMessage message={formik.errors.password} />
-          )}
-        </View>
-        {stateAuth.errorMessage && (
-          <ErrorMessage message={stateAuth.errorMessage} />
-        )}
-
-        <View>
-          <TouchableOpacity
-            style={[
-              globalThemes.defaultBtn,
-              {
-                backgroundColor: state.colors.primary,
-                borderColor: state.colors.border,
-              },
-            ]}
-            onPress={formik.handleSubmit}
-          >
+          <View>
             <Text
-              style={[
-                globalThemes.defaulTextBtn,
-                { color: state.colors.title },
-              ]}
+              style={[globalThemes.title, { color: state.colors.titleColor }]}
             >
-              GUARDAR
+              Bienvenid@s
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View>
+            <Text style={[globalThemes.text, { color: state.colors.text }]}>
+              Por favor registrese para continuar navegando en nuestra app.
+            </Text>
+          </View>
+          <View style={state.container}>
+            <View>
+              <TextInput
+                style={[
+                  globalThemes.defaultInputText,
+                  {
+                    color: state.colors.text,
+                    borderColor: state.colors.border,
+                  },
+                ]}
+                maxLength={20}
+                placeholder="Nombre"
+                placeholderTextColor={state.colors.notification}
+                name="firstname"
+                value={formik.values.firstname}
+                onChangeText={(value) =>
+                  formik.setFieldValue("firstname", value)
+                }
+              />
+              {formik.errors.firstname && (
+                <ErrorMessage message={formik.errors.firstname} />
+              )}
+              <TextInput
+                style={[
+                  globalThemes.defaultInputText,
+                  {
+                    color: state.colors.text,
+                    borderColor: state.colors.border,
+                  },
+                ]}
+                maxLength={20}
+                placeholder="Apellido"
+                placeholderTextColor={state.colors.notification}
+                name="lastname"
+                value={formik.values.lastname}
+                onChangeText={(value) =>
+                  formik.setFieldValue("lastname", value)
+                }
+              />
+              {formik.errors.lastname && (
+                <ErrorMessage message={formik.errors.lastname} />
+              )}
+              <TextInput
+                style={[
+                  globalThemes.defaultInputText,
+                  {
+                    color: state.colors.text,
+                    borderColor: state.colors.border,
+                  },
+                ]}
+                maxLength={40}
+                placeholder="Correo"
+                placeholderTextColor={state.colors.notification}
+                keyboardType="email-address"
+                name="email"
+                value={formik.values.email}
+                onChangeText={(value) => formik.setFieldValue("email", value)}
+              />
+              {formik.errors.email && (
+                <ErrorMessage message={formik.errors.email} />
+              )}
+
+              <TextInput
+                style={[
+                  globalThemes.defaultInputText,
+                  {
+                    color: state.colors.text,
+                    borderColor: state.colors.border,
+                  },
+                ]}
+                maxLength={30}
+                placeholder="Contraseña"
+                placeholderTextColor={state.colors.notification}
+                secureTextEntry={true}
+                name="password"
+                value={formik.values.password}
+                onChangeText={(value) =>
+                  formik.setFieldValue("password", value)
+                }
+              />
+              {formik.errors.password && (
+                <ErrorMessage message={formik.errors.password} />
+              )}
+            </View>
+            {stateAuth.errorMessage && (
+              <ErrorMessage message={stateAuth.errorMessage} />
+            )}
+
+            <View>
+              <TouchableOpacity
+                style={[
+                  globalThemes.defaultBtn,
+                  {
+                    backgroundColor: !(formik.isValid && formik.dirty)
+                      ? state.colors.notification
+                      : state.colors.primary,
+                    borderColor: state.colors.border,
+                  },
+                ]}
+                disabled={!(formik.isValid && formik.dirty)}
+                onPress={formik.handleSubmit}
+              >
+                <Text
+                  style={[
+                    globalThemes.defaulTextBtn,
+                    {
+                      color: state.colors.title,
+                    },
+                  ]}
+                >
+                  GUARDAR
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={[globalThemes.footer, { color: state.colors.text }]}>
+                RollingAppRN
+              </Text>
+            </View>
+          </View>
         </View>
-        <View>
-          <Text style={[globalThemes.footer, { color: state.colors.text }]}>
-            RollingAppRN
-          </Text>
-        </View>
-      </View>
+      )}
     </>
   );
 };
@@ -201,14 +234,14 @@ export const RegisterScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   toastContainer: {
     height: 60,
-    width: '100%',
-    backgroundColor: '#f5f5f5',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10,
     marginHorizontal: 5,
 
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
